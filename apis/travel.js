a7a515916a285903f7df3c1533ae0f554f7af9ce
swellee <shiwei_li@odao.com>
2018-03-13 20:55:04 +0800

    up

---
+++ apis/travel.js
@@ -0,0 +1,86 @@
+//-------------enums---------------
+class RankType{
+    
+    static get DAY() { return 1;}
+    
+    static get MONTH() { return 3;}
+    
+    static get RECHARGE() { return 9;}
+    
+}
+//------------classes--------------
+class RankItem  {
+    constructor(){
+    
+        //prop type: string
+        this.name = null;
+    
+        //prop type: number
+        this.rank = null;
+    
+        this.reqFields = [];
+        this.resFields = [];
+    }
+}
+class RankInfo  {
+    constructor(){
+    
+        this._rankType = null;
+        this._limit = null;
+        this._selfRank = null;
+        this._ranks = null;
+        this.reqFields = ["rankType","limit"];
+        this.resFields = ["selfRank","ranks"];
+    }
+    //client input, require, type: RankType
+    get rankType() {return this._rankType}
+    set rankType(v) {this._rankType = v}
+    //client input, optional, type: number
+    get limit() {return this._limit}
+    set limit(v) {this._limit = v}
+    //server output, type: number
+    get selfRank() {return this._selfRank}
+    set selfRank(v) {this._selfRank = v}
+    //server output, type: RankItem[]
+    get ranks() {return this._ranks}
+    set ranks(v) {this._ranks = v}
+}
+class Base  {
+    constructor(){
+    
+        //prop type: string
+        this.action = null;
+    
+        this.reqFields = [];
+        this.resFields = [];
+    }
+   submit() {
+        let tmp ={};
+        tmp.action=this.action;
+        this.resFields.forEach(k => {
+           tmp[k]=this[k]
+        });
+
+        this.ctx.body=tmp;
+    }
+   parse(data) {
+        Object.assign(this, data);
+    }
+}
+class RechargeRankInfo extends RankInfo {
+    constructor(){
+        super();
+        this._myRecharge = null;
+        this.reqFields = ["rankType","limit"];
+        this.resFields = ["myRecharge","selfRank","ranks"];
+    }
+    //server output, type: number
+    get myRecharge() {return this._myRecharge}
+    set myRecharge(v) {this._myRecharge = v}
+}
+//-------------exports---------------
+exports.RankType = RankType;
+exports.RankItem = RankItem;
+exports.RankInfo = RankInfo;
+exports.Base = Base;
+exports.RechargeRankInfo = RechargeRankInfo;
--- app/tools/genApi/config.js
+++ app/tools/genApi/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     //注：配的相对路径，是相对于本项目根路径，即‘./’表示package.json所在的目录
-    english: {
+    travel: {
         enable: true,
-        inDir: './app/tools/genApi/apis/english',//可以是目录，也可以是具体到某个文件,文件
-        outFileC: '../english/api.js',
-        outFileS: './apis/english.js'
+        inDir: './app/tools/genApi/apis/travel',//可以是目录，也可以是具体到某个文件,文件
+        outFileC: '../travel/api.js',
+        outFileS: './apis/travel.js'
     }
 }
