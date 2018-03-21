567d4aa312d70bfd21d314d28722d6fffd408b7e
h5group <h5group@h5group-swelleedeMac.local>
2018-03-21 11:07:42 +0800

    p

--- app/service/travelService/travelService.js
+++ app/service/travelService/travelService.js
@@ -3,7 +3,7 @@ const Service = require('egg').Service;
 module.exports = app => {
     return class TravelService extends Service {
         async fillIndexInfo(info) {
-            //info typeof apis.IndexInfo
+            // info typeof apis.IndexInfo
             let usr = await this.ctx.model.PublicModel.User.find({uid: info.uid});
             info.isFirst = usr.isFirst;
 
@@ -12,4 +12,5 @@ module.exports = app => {
             info.playerCnt = await this.ctx.service.publicService.userService.getPlayerCnt();
         }
     }
+
 };
