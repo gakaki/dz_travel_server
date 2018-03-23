2def30ca3a1859a426813eb9ca5b73492f70a2cc
zhangqi <qii_zhang@odao.com>
2018-03-23 13:30:44 +0800

    up

--- apis/travel.js
+++ apis/travel.js
@@ -40,6 +40,66 @@ class PostType{
     
 }
 //------------classes--------------
+class Post  {
+    constructor(){
+    
+        //prop type: number//帖子id
+        this.postId = null;
+    
+        //prop type: PostType//帖子类型：景点or特产
+        this.type = null;
+    
+        //prop type: string//帖子内容，为景点或特产的介绍
+        this.content = null;
+    
+        //prop type: 
+        this.name = null;
+    
+        //prop type: number//帖子的评论
+        this.score = null;
+    
+        //prop type: string//景点或特产图片url
+        this.img = null;
+    
+        //prop type: number//评论数
+        this.commentNum = null;
+    
+        
+        
+    }
+}
+class RankItem  {
+    constructor(){
+    
+        //prop type: string
+        this.name = null;
+    
+        //prop type: number
+        this.rank = null;
+    
+        //prop type: userInfo
+        this.userInfo = null;
+    
+        
+        
+    }
+}
+class Provence  {
+    constructor(){
+    
+        //prop type: 
+        this.proLetter = null;
+    
+        //prop type: 
+        this.provence = null;
+    
+        //prop type: 
+        this.citys = null;
+    
+        
+        
+    }
+}
 class Base  {
     constructor(){
     
@@ -73,19 +133,6 @@ class Base  {
         Object.assign(this, data);
     }
 }
-class RankItem  {
-    constructor(){
-    
-        //prop type: string
-        this.name = null;
-    
-        //prop type: number
-        this.rank = null;
-    
-        
-        
-    }
-}
 class UserBriefInfo  {
     constructor(){
     
@@ -97,28 +144,6 @@ class UserBriefInfo  {
     
         //prop type: string
         this.avatarUrl = null;
-    
-        
-        
-    }
-}
-class Post  {
-    constructor(){
-    
-        //prop type: number//帖子id
-        this.postId = null;
-    
-        //prop type: PostType//帖子类型：景点or特产
-        this.type = null;
-    
-        //prop type: string//帖子内容，为景点或特产的介绍
-        this.content = null;
-    
-        //prop type: number//创建时间
-        this.time = null;
-    
-        //prop type: string//景点或特产图片url
-        this.img = null;
     
         
         
@@ -152,75 +177,14 @@ class Comment  {
         
     }
 }
-class RankInfo extends Base {
+class City  {
     constructor(){
-        super();
-        this._rankType = null;
-        this._limit = null;
-        this._selfRank = null;
-        this._ranks = null;
-        this.reqFields = ["rankType","limit"];
-        this.resFields = ["selfRank","ranks"];
-    }
-    //client input, require, type: RankType
-    get rankType() {return this._rankType}
-    set rankType(v) {this._rankType = v}
-    //client input, optional, type: number
-    get limit() {return this._limit}
-    set limit(v) {this._limit = v}
-    //server output, type: number
-    get selfRank() {return this._selfRank}
-    set selfRank(v) {this._selfRank = v}
-    //server output, type: RankItem[]
-    get ranks() {return this._ranks}
-    set ranks(v) {this._ranks = v}
-    static Init(ctx) {
-    let o = new RankInfo();
-    o.ctx = ctx;
-    o.parse(ctx.query);
-    return o;
-    }
-}
-class PostComments extends Base {
-    constructor(){
-        super();
-        this._comments = null;
-        this.reqFields = [];
-        this.resFields = ["comments"];
-    }
-    //server output, type: Comment[]//该帖子下的评论
-    get comments() {return this._comments}
-    set comments(v) {this._comments = v}
-    static Init(ctx) {
-    let o = new PostComments();
-    o.ctx = ctx;
-    o.parse(ctx.query);
-    return o;
-    }
-}
-class UserInfo extends UserBriefInfo {
-    constructor(){
-        super();
-        //prop type: string
-        this.gender = null;
     
-        //prop type: string
-        this.city = null;
+        //prop type: 
+        this.cityname = null;
     
-        //prop type: string
-        this.province = null;
-    
-        //prop type: string
-        this.country = null;
-    
-        //prop type: Boolean
-        this.online = null;
-    
-        //prop type: KV[]
-        this.items = null;
-    
-        //prop type: string[]
-        this.friends = null;
+        //prop type: 
+        this.cityper = null;
     
         
         
@@ -284,6 +248,35 @@ class IndexInfo extends Base {
     return o;
     }
 }
+class RankInfo extends Base {
+    constructor(){
+        super();
+        this._rankType = null;
+        this._limit = null;
+        this._selfRank = null;
+        this._ranks = null;
+        this.reqFields = ["rankType","limit"];
+        this.resFields = ["selfRank","ranks"];
+    }
+    //client input, require, type: RankType
+    get rankType() {return this._rankType}
+    set rankType(v) {this._rankType = v}
+    //client input, optional, type: number
+    get limit() {return this._limit}
+    set limit(v) {this._limit = v}
+    //server output, type: number
+    get selfRank() {return this._selfRank}
+    set selfRank(v) {this._selfRank = v}
+    //server output, type: RankItem[]
+    get ranks() {return this._ranks}
+    set ranks(v) {this._ranks = v}
+    static Init(ctx) {
+    let o = new RankInfo();
+    o.ctx = ctx;
+    o.parse(ctx.query);
+    return o;
+    }
+}
 class PostList extends Base {
     constructor(){
         super();
@@ -314,6 +307,23 @@ class CommentPost extends Base {
     return o;
     }
 }
+class PostComments extends Base {
+    constructor(){
+        super();
+        this._comments = null;
+        this.reqFields = [];
+        this.resFields = ["comments"];
+    }
+    //server output, type: Comment[]//该帖子下的评论
+    get comments() {return this._comments}
+    set comments(v) {this._comments = v}
+    static Init(ctx) {
+    let o = new PostComments();
+    o.ctx = ctx;
+    o.parse(ctx.query);
+    return o;
+    }
+}
 class ThumbComment extends Base {
     constructor(){
         super();
@@ -322,6 +332,51 @@ class ThumbComment extends Base {
     }
     static Init(ctx) {
     let o = new ThumbComment();
+    o.ctx = ctx;
+    o.parse(ctx.query);
+    return o;
+    }
+}
+class UserInfo extends UserBriefInfo {
+    constructor(){
+        super();
+        //prop type: string
+        this.gender = null;
+    
+        //prop type: string
+        this.city = null;
+    
+        //prop type: string
+        this.province = null;
+    
+        //prop type: string
+        this.country = null;
+    
+        //prop type: Boolean
+        this.online = null;
+    
+        //prop type: KV[]
+        this.items = null;
+    
+        //prop type: string[]
+        this.friends = null;
+    
+        
+        
+    }
+}
+class cityList extends Base {
+    constructor(){
+        super();
+        this._provence = null;
+        this.reqFields = [];
+        this.resFields = ["provence"];
+    }
+    //server output, type: Provence[]
+    get provence() {return this._provence}
+    set provence(v) {this._provence = v}
+    static Init(ctx) {
+    let o = new cityList();
     o.ctx = ctx;
     o.parse(ctx.query);
     return o;
@@ -349,17 +404,20 @@ exports.Season = Season;
 exports.Weather = Weather;
 exports.RankType = RankType;
 exports.PostType = PostType;
-exports.Base = Base;
-exports.RankItem = RankItem;
-exports.UserBriefInfo = UserBriefInfo;
 exports.Post = Post;
+exports.RankItem = RankItem;
+exports.Provence = Provence;
+exports.Base = Base;
+exports.UserBriefInfo = UserBriefInfo;
 exports.Comment = Comment;
-exports.RankInfo = RankInfo;
-exports.PostComments = PostComments;
-exports.UserInfo = UserInfo;
+exports.City = City;
 exports.PlayerInfo = PlayerInfo;
 exports.IndexInfo = IndexInfo;
+exports.RankInfo = RankInfo;
 exports.PostList = PostList;
 exports.CommentPost = CommentPost;
+exports.PostComments = PostComments;
 exports.ThumbComment = ThumbComment;
+exports.UserInfo = UserInfo;
+exports.cityList = cityList;
 exports.RechargeRankInfo = RechargeRankInfo;
