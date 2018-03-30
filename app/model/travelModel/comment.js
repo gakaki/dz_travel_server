603a2d07c3d1263f432470dc5905a21962cc24b9
qianjun <727435849@qq.com>
2018-03-30 16:18:29 +0800

    model

--- app/model/travelModel/comment.js
+++ app/model/travelModel/comment.js
@@ -3,7 +3,7 @@ module.exports = app => {
     const CommentSchema = new mongoose.Schema({
         uid:{type:String},
         scenicspot:{type:String},//景点id
-        commenttype:{type:Number,enum:[0,1]},//0 攻略 1 特产
+        commenttype:{type:Number,enum:[1,2]},//1 攻略 2 特产
         travel_tips:{type:String}, //攻略特产id
         comid:{type:String}, //评论id
         context:{tyep:String}, //内容
