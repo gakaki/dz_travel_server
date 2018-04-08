4871b74bcaf997bee9c96092d979f331b9e26bca
swellee <shiwei_li@odao.com>
2018-04-08 15:29:56 +0800

    up

--- app/controller/travelController/specialityController.js
+++ app/controller/travelController/specialityController.js
@@ -4,18 +4,18 @@ const utils = require("../../utils/utils");
 const sheets = require("../../../sheets/travel");
 
 //特产买卖相关的
-class SpecialtyController extends Controller {
+class SpecialityController extends Controller {
     //城市特产列表
     async cityspes(ctx) {
         let info = apis.CitySpes.Init(ctx);
-        await ctx.service.travelService.specialtyService.citySpecialtys(info);
+        await ctx.service.travelService.specialityService.cityspes(info);
         info.submit();
     }
 
     //我拥有的特产列表
     async myspes(ctx) {
         let info = apis.MySpes.Init(ctx);
-        await ctx.service.travelService.specialtyService.mySpecialtys(info);
+        await ctx.service.travelService.specialityService.myspes(info);
         info.submit();
     }
 
@@ -23,17 +23,17 @@ class SpecialtyController extends Controller {
     async buy(ctx) {
         //买入特产的id 数量
         let info = await apis.BuySpe.Init(ctx, true);
-        await ctx.service.travelService.specialtyService.buy(info);
+        await ctx.service.travelService.specialityService.buy(info);
         info.submit();
     }
     //卖出特产
     async sell(ctx) {
         //卖出特产的id 数量
         let info = await apis.SellSpe.Init(ctx, true);
-        await ctx.service.travelService.specialtyService.sell(info);
+        await ctx.service.travelService.specialityService.sell(info);
         info.submit();
     }
 
 
 }
-module.exports = SpecialtyController;
+module.exports = SpecialityController;
