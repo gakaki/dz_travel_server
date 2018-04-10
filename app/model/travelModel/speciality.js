445ab54be31e1d6c58b4fe4c5cea4f40f4b2de2b
swellee <shiwei_li@odao.com>
2018-04-10 20:59:10 +0800

    up

--- app/model/travelModel/speciality.js
+++ app/model/travelModel/speciality.js
@@ -1,10 +1,14 @@
 module.exports = app => {
     const mongoose = app.mongoose;
     const SpecialitySchema = new mongoose.Schema({
+        cid: {type: String}, //所属城市id
         uid:{type:String},
         spid:{type:String},   //特产ID
+        price: {type: Number}, //买入价格
+        sellPrice: {type: Number, default: 0}, //异地出售价格
         number:{type:Number, default: 0},
-        createDate:{type:Date} //获取时间
+        createDate:{type:Date}, //获取时间
+        sellPriceDate: {type: Date, default: new Date()} //价格刷新时间
     });
 
     return mongoose.model('Speciality',SpecialitySchema);
