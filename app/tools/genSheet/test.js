e3ff5bbe0bbcd4f11a5a2d5b01d5489ee3cddd15
zhangqi <qii_zhang@odao.com>
2018-04-10 14:46:33 +0800

    up

--- app/tools/genSheet/test.js
+++ app/tools/genSheet/test.js
@@ -1,10 +1,19 @@
-const travelConfig = require("../../../sheets/travel")
+const travelConfig = require("../../../sheets/travel");
+const ScenicPos = require("../../../sheets/scenicpos");
 const moment = require("moment");
 const holidayCn = require('holiday.cn').default;
 
 
-
-let context = travelConfig.Message.Get(travelConfig.Message.POSTCARDMESSAGE).content;
-let content = context.replace("s%","haha");
-console.log(context);
-console.log(content);
+let city = travelConfig.City.Get(1);
+city.scenicspot.map((s, idx) => {
+    let o = {};
+    let cfg = travelConfig.Scenicspot.Get(s);
+    let xy = ScenicPos.Get(s);
+    o.id = s;
+    o.cid = cid;
+    o.name = cfg.scenicspot;
+    o.building = cfg.building;
+    o.x = xy.x;
+    o.y = xy.y;
+    o.index = idx;// 真实情况，应该读库
+}),
