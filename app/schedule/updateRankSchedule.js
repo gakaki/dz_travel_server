b6344bec17da7a1618d25dc628b3c597aaee835a
qianjun <727435849@qq.com>
2018-04-12 16:36:56 +0800

    rank

--- app/schedule/updateRankSchedule.js
+++ app/schedule/updateRankSchedule.js
@@ -6,8 +6,9 @@ class UpdateRankSchedule extends Subscription {
         return {
             type: 'worker', // 一个 worker 执行
             //每天23:30执行一次
-             cron: '0 0 0 * * *'// s m h dayOfMonth month dayOfWeek; see http://eggjs.org/zh-cn/basics/schedule.html
+             cron: '0 0 0 * * *',// s m h dayOfMonth month dayOfWeek; see http://eggjs.org/zh-cn/basics/schedule.html
            // interval: '3m'// 本地测试，每3分钟生成一次
+            immediate: true,
         };
     }
 
