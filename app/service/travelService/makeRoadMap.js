8b7dda4f4fdb0319743a088a564b81d29f369a2c
zhangqi <qii_zhang@odao.com>
2018-04-18 10:28:34 +0800

    up

--- app/service/travelService/makeRoadMap.js
+++ app/service/travelService/makeRoadMap.js
@@ -1,22 +1,23 @@
-const _                     = require("lodash");
-const travelConfig          = require("../../../sheets/travel");
-const QuestRepo             = require("../questService/questRepo");
-const ScenicPos             = require("../../../sheets/scenicpos");
-const timeUtil              = require("../../utils/time");
-const apis                  = require("../../../apis/travel");
+const _ = require("lodash");
+const travelConfig = require("../../../sheets/travel");
+const QuestRepo = require("../questService/questRepo");
+const ScenicPos = require("../../../sheets/scenicpos");
+const timeUtil = require("../../utils/time");
+const apis = require("../../../apis/travel");
+
 // 生成路线
 class MakeRoadMap {
 
-    constructor( obj ){
-        this.oldLine      = obj.oldLine;
-        this.spotIds        = obj.line || [];
-        this.isNewPlayer    = obj.isNewPlayer || 0; //新手引导总时间会变成1
-        this.cid            = obj.cid || 0;
+    constructor(obj) {
+        this.oldLine = obj.oldLine;
+        this.spotIds = obj.line || [];
+        this.isNewPlayer = obj.isNewPlayer || 0; //新手引导总时间会变成1
+        this.cid = obj.cid || 0;
 
-        this.lines          = []; //所有线路容器
-        this.linesFormat    = []; //返回给前端用的
+        this.lines = []; //所有线路容器
+        this.linesFormat = []; //返回给前端用的
 
-        this.rentItems      =obj.rentItems || 0;
+        this.rentItems = obj.rentItems || 0;
         this.clacSpeed();        // 时间配置
         this.setSpotsCfg();
         this.calcTimeTotal();    // 计算和返回line的时间点
@@ -24,38 +25,37 @@ class MakeRoadMap {
     }
 
 
-
-    formatOutput(){
+    formatOutput() {
         let formatRoadMap = [];
-        for ( let line of this.lines ){
+        for (let line of this.lines) {
             let row = {};
 
-            let spotStart       = line['spotStart'];
-            let spotEnd         = line['spotEnd'];
-            spotEnd['endtime']  = line['timeEnd'];
+            let spotStart = line['spotStart'];
+            let spotEnd = line['spotEnd'];
+            spotEnd['endtime'] = line['timeEnd'];
 
 
-            if ( this.lines.indexOf(line) == 0 ) {
+            if (this.lines.indexOf(line) == 0) {
                 formatRoadMap.push(spotStart);
                 formatRoadMap.push(spotEnd);
-            }else{
+            } else {
                 formatRoadMap.push(spotEnd);
             }
         }
 
-       // console.log(formatRoadMap);
+        // console.log(formatRoadMap);
 
-        for ( let line of formatRoadMap ){
-            if ( line['isStart'] == 1) {
-                line['arriveStamp']         = line['startime'];
-                line['arriveStampYMDHMS']   = timeUtil.formatYMDHMS(line['startime']);
-            }else{
-                line['arriveStamp']         = line['endtime'];
-                line['arriveStampYMDHMS']   = timeUtil.formatYMDHMS(line['endtime']);
+        for (let line of formatRoadMap) {
+            if (line['isStart'] == 1) {
+                line['arriveStamp'] = line['startime'];
+                line['arriveStampYMDHMS'] = timeUtil.formatYMDHMS(line['startime']);
+            } else {
+                line['arriveStamp'] = line['endtime'];
+                line['arriveStampYMDHMS'] = timeUtil.formatYMDHMS(line['endtime']);
             }
         }
 
-        this.linesFormat        = formatRoadMap;
+        this.linesFormat = formatRoadMap;
         // this.linesFormat        = {
         //     timeTotalHour : this.timeTotalHour,
         //     roadMap   : formatRoadMap
@@ -63,124 +63,122 @@ class MakeRoadMap {
 
     }
 
-    clacSpeed(){
-        let all_time                 = 24;                              //6个点5个路程线 总小时 小时为单位 按照步行的速度算
+    clacSpeed() {
+        let all_time = 24;                              //6个点5个路程线 总小时 小时为单位 按照步行的速度算
         //总时间                = 计算该城市有几个line * 单独一个line 4.8个小时 普通走 总时间
         //剩下时间              = （总时间 - 当前行走了多少时间 ） * 0.6
         //总时间新手引导         修改为1个小时
         //当前行走了多少时间 * humanSpeed
-        let timeHumanPreLineHour     = all_time / ( 6 - 1 );       //一段line 4.8小时
+        let timeHumanPreLineHour = all_time / (6 - 1);       //一段line 4.8小时
         // 正常走路时间
-        this.timeHumanPreLineHour    = timeHumanPreLineHour;
-        if(this.rentItems) {
+        this.timeHumanPreLineHour = timeHumanPreLineHour;
+        if (this.rentItems) {
             let shortTime = [];
-            for(let rentItem in this.rentItems) {
-                if(this.rentItems[rentItem]) {
+            for (let rentItem in this.rentItems) {
+                if (this.rentItems[rentItem]) {
                     let item = travelConfig.Shop.Get(rentItem);
-                    if(item && item.type == apis.RentItem.CAR) {
+                    if (item && item.type == apis.RentItem.CAR) {
                         shortTime.push(item.value);
                     }
                 }
             }
-            if(shortTime.length > 0 ) {
+            if (shortTime.length > 0) {
                 this.timeHumanPreLineHour = parseFloat((timeHumanPreLineHour * Math.max(...shortTime) / 100).toFixed(2));
             }
-          //  this.timeCarGreat            = timeHumanPreLineHour * 0.6;            // 2.88  	豪华自驾车	租赁豪华自驾车，可缩短60%本城市旅行时间。	1001
-         //   this.timeCarMedium           = timeHumanPreLineHour * 0.5;            // 2.4    舒适自驾车	租赁舒适自驾车，可缩短50%本城市旅行时间。	1002
-         //   this.timeCarCheap            = timeHumanPreLineHour * 0.4;            // 1.92   经济自驾车	租赁经济自驾车，可缩短40%本城市旅行时间。	1003
+            //  this.timeCarGreat            = timeHumanPreLineHour * 0.6;            // 2.88  	豪华自驾车	租赁豪华自驾车，可缩短60%本城市旅行时间。	1001
+            //   this.timeCarMedium           = timeHumanPreLineHour * 0.5;            // 2.4    舒适自驾车	租赁舒适自驾车，可缩短50%本城市旅行时间。	1002
+            //   this.timeCarCheap            = timeHumanPreLineHour * 0.4;            // 1.92   经济自驾车	租赁经济自驾车，可缩短40%本城市旅行时间。	1003
         }
 
     }
-    calcTimeTotal(){
 
-        let lineCount                = this.spotIds.length - 1;    // 几个点-1 为多少个line 线
-        let timeTotal                = lineCount * this.timeHumanPreLineHour; //无道具正常时间 小时单位
+    calcTimeTotal() {
+
+        let lineCount = this.spotIds.length - 1;    // 几个点-1 为多少个line 线
+        let timeTotal = lineCount * this.timeHumanPreLineHour; //无道具正常时间 小时单位
 
         // 计算总时间 并生成具体的 line线
         this.setLines();
 
-        this.timeTotalHour           = 0;
-        for (let l of this.lines){
-            this.timeTotalHour     += l['timeElapsed'];
+        this.timeTotalHour = 0;
+        for (let l of this.lines) {
+            this.timeTotalHour += l['timeElapsed'];
         }
 
         // this.timeTotalHour           = this.lines.map( s => s.timeElapsed ).reduce( (pre,cur) => {return pre + cur} );
-        if ( this.isNewPlayer ){    //新手引导总时间变为1
-            this.timeTotalHour       = 1;
+        if (this.isNewPlayer) {    //新手引导总时间变为1
+            this.timeTotalHour = 1;
         }
-        this.timeTotalHour           = parseFloat((this.timeTotalHour).toPrecision(4))
+        this.timeTotalHour = parseFloat((this.timeTotalHour).toPrecision(4))
     }
 
-    setSpotsCfg(){
+    setSpotsCfg() {
         let hasRouter = [];
-       // let spotsCityStart = null;
-        this.spotsCfg        = this.spotIds.map( (spotId,index) => {
-            let cfg          = travelConfig.Scenicspot.Get(spotId);
-            let xy           = ScenicPos.Get(spotId);
-            let o            = {};
+        // let spotsCityStart = null;
+        this.spotsCfg = this.spotIds.map((spotId, index) => {
+            let cfg = travelConfig.Scenicspot.Get(spotId);
+            let xy = ScenicPos.Get(spotId);
+            let o = {};
 
-           // let oldindex = this.oldLine.findIndex((n) => n.id == spotId);
+            // let oldindex = this.oldLine.findIndex((n) => n.id == spotId);
             let old = this.oldLine.find((n) => n.id == spotId);
-            if(!old || old.index == -1) {
-                o.id             = spotId;
-                o.cid            = this.cid;
-                o.name           = cfg.scenicspot;
-                o.building       = cfg.building;
-                o.x              = xy.x;
-                o.y              = xy.y;
-                o.tracked        = false;
-                o.index          = index;
-                o.startime      ="";
-                o.endtime        = "";
-                let [lng,lat]    = cfg["coordinate"];
-                o.lng            = lng;
-                o.lat            = lat;
-            }else{
+            if (!old || old.index == -1) {
+                o.id = spotId;
+                o.cid = this.cid;
+                o.name = cfg.scenicspot;
+                o.building = cfg.building;
+                o.x = xy.x;
+                o.y = xy.y;
+                o.tracked = false;
+                o.index = index;
+                o.startime = "";
+                o.endtime = "";
+                let [lng, lat] = cfg["coordinate"];
+                o.lng = lng;
+                o.lat = lat;
+            } else {
                 hasRouter.push(old.index);
                 o = old;
             }
             return o;
         });
-       if(hasRouter.length == 0) {
-           //忘记加上起始点的了 加上起始点
-           let cityConfig      = travelConfig.City.Get(this.cid);
-           let [lng,lat]        = cityConfig["coordinate"];
-           let spotsCityStart   = {
-               id             : -1, //-1 表示是起点
-               cid            : this.cid,
-               name           : "城市的起点",
-               x              : 0,   //这里的不算数
-               y              : 0,
-               tracked        : true,  //起点肯定默认就到达了
-               index          : 0,     //这个index 有必要吗
-               startime      : "",    //开始时间
-               endtime        : "",    //结束时间
-               lng            : lng,
-               lat            : lat,
-               isStart        : true //是否起点
-           };
-           this.spotsCfg.unshift(spotsCityStart);
-       }
-
-
-
+        if (hasRouter.length == 0) {
+            //忘记加上起始点的了 加上起始点
+            let cityConfig = travelConfig.City.Get(this.cid);
+            let [lng, lat] = cityConfig["coordinate"];
+            let spotsCityStart = {
+                id: -1, //-1 表示是起点
+                cid: this.cid,
+                name: "城市的起点",
+                x: 0,   //这里的不算数
+                y: 0,
+                tracked: true,  //起点肯定默认就到达了
+                index: 0,     //这个index 有必要吗
+                startime: "",    //开始时间
+                endtime: "",    //结束时间
+                lng: lng,
+                lat: lat,
+                isStart: true //是否起点
+            };
+            this.spotsCfg.unshift(spotsCityStart);
+        }
 
 
     }
 
-    setLines(){
-        let linesTotal      = [];
-        this.spotsCfg.reduce( (prev, current, index, arr) => {
+    setLines() {
+        let linesTotal = [];
+        this.spotsCfg.reduce((prev, current, index, arr) => {
 
-            let line        = this.makeLine(prev, current);
+            let line = this.makeLine(prev, current);
             linesTotal.push(line);
-            if ( index == arr.length - 1 ){
+            if (index == arr.length - 1) {
 
-            }else{
+            } else {
                 return current
             }
         });
-        this.lines     = linesTotal;
+        this.lines = linesTotal;
     }
 
     // 计算每段line的时间
@@ -190,39 +188,39 @@ class MakeRoadMap {
         // let timeElapsed = parseInt(dis / this.speed);
         // 不用距离算法了
         // 中途换道具在写
-        let timeHour    = this.timeHumanPreLineHour;
-       let diffTime    = timeHour * 60 * 60 * 1000;
-       //  let diffTime    =  1000;
+        let timeHour = this.timeHumanPreLineHour;
+        //let diffTime    = timeHour * 60 * 60 * 1000;
+        let diffTime = 5000;
 
 
         if (spotStart['isStart'] == true) {
-            if(!spotStart['startime']) {
-                let start       = new Date().getTime();
-                let end         = start + diffTime;
+            if (!spotStart['startime']) {
+                let start = new Date().getTime();
+                let end = start + diffTime;
 
-                spotStart['startime']   = start;
-                spotEnd['endtime']      = end;
+                spotStart['startime'] = start;
+                spotEnd['endtime'] = end;
             }
-        }else{
-            if(!spotStart['startime']) {
-                spotStart['startime']   = spotStart['endtime'];
-                spotEnd['endtime']      = spotStart['endtime'] + diffTime;
+        } else {
+            if (!spotStart['startime']) {
+                spotStart['startime'] = spotStart['endtime'];
+                spotEnd['endtime'] = spotStart['endtime'] + diffTime;
             }
 
         }
 
 
-        let line        = {
+        let line = {
             // distance    : dis,
-            timeElapsed : timeHour,
-            timeStart   : spotStart['startime'],
-            timeEnd     : spotEnd['endtime'],
-            spotStart   : spotStart,
-            spotEnd     : spotEnd,
-            spotIdStart : spotStart.id,
-            spotIdEnd   : spotEnd.id,
-            timeStartFull : spotStart['startime'] ? timeUtil.formatYMDHMS(spotStart['startime']) : spotStart['startime'],
-            timeStartEnd  : spotEnd['endtime'] ? timeUtil.formatYMDHMS(spotEnd['endtime']) : spotEnd['endtime'],
+            timeElapsed: timeHour,
+            timeStart: spotStart['startime'],
+            timeEnd: spotEnd['endtime'],
+            spotStart: spotStart,
+            spotEnd: spotEnd,
+            spotIdStart: spotStart.id,
+            spotIdEnd: spotEnd.id,
+            timeStartFull: spotStart['startime'] ? timeUtil.formatYMDHMS(spotStart['startime']) : spotStart['startime'],
+            timeStartEnd: spotEnd['endtime'] ? timeUtil.formatYMDHMS(spotEnd['endtime']) : spotEnd['endtime'],
         }
         return line;
     }
@@ -243,10 +241,6 @@ module.exports = MakeRoadMap;
 // console.log(er.lines);
 // console.log(er.timeTotalHour);
 // console.log(er.linesFormat);
-
-
-
-
 
 
 // console.log(new Date() /1000 * 1000);
@@ -293,7 +287,6 @@ module.exports = MakeRoadMap;
 //redis 记录上一次的持续时间 注意数据库需要恢复哦
 //redis 记录用户的状态
 //计算和上一次的触发时间到底差多少
-
 
 
 // var arr = [1,2,3,4];
