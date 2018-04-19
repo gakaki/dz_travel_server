494d9ec3b29f96ab602c83245f33857fef8bbcd5
yhy <405939579@qq.com>
2018-04-19 19:11:53 +0800

    up

--- apis/travel.js
+++ apis/travel.js
@@ -1071,6 +1071,7 @@ class TourIndexInfo extends Base {
         this.action = 'tour.tourindexinfo';
     
         this._cid = null;
+        this._isDouble = null;
         this._weather = null;
         this._spots = null;
         this._task = null;
@@ -1079,13 +1080,16 @@ class TourIndexInfo extends Base {
         this._display = null;
         this._startTime = null;
         this._partener = null;
-        this.requireFileds = ["cid"];
-        this.reqFields = ["cid"];
+        this.requireFileds = ["cid","isDouble"];
+        this.reqFields = ["cid","isDouble"];
         this.resFields = ["weather","spots","task","startPos","others","display","startTime","partener"];
     }
     //client input, require, type: number
     get cid() {return this._cid}
     set cid(v) {this._cid = v}
+    //client input, require, type: boolean
+    get isDouble() {return this._isDouble}
+    set isDouble(v) {this._isDouble = v}
     //server output, type: number
     get weather() {return this._weather}
     set weather(v) {this._weather = v}
