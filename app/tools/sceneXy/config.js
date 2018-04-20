2e4538e6c41103ed60cb766fcddac06e1405a115
swellee <shiwei_li@odao.com>
2018-04-20 14:55:21 +0800

    up

--- app/tools/sceneXy/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    viewWidth: 750, //用于展示游玩景点的视图宽
-    viewHeight: 1200, //用于展示游玩景点的视图高
-    paddingTop: 0, //内边距-上
-    paddingBottom: 100, //内边距-下
-    paddingLeft: 20, //内边距-左
-    paddingRight: 60, //内边距-右
-    outFile: './sheets/scenicpos.js', //输出文件
-}
---
+++ app/tools/scenicPic/checkPic.js
@@ -0,0 +1,63 @@
+const config = require('./config')
+const fs = require('fs')
+const path = require('path');
+const sheets = require('../../../sheets/travel')
+
+//-----list files
+let files = {};
+let result = {};
+let rootDir = path.join(config.picBaseDir, config.picDirName);
+recFile(rootDir);
+
+compare();
+
+function recFile(f) {
+    let st = fs.statSync(f);
+    if (st.isFile()) {
+        let key = path.join(config.picDirName, f.split(config.picDirName)[1]).replace(/\\+/g, '/')
+        files[key] = 1;
+    }
+    else {
+        //directory
+        fs.readdirSync(f).every(ff => {
+            let subFl = path.join(f, ff);
+            recFile(subFl);
+            return true;
+        })
+    }
+}
+
+function compare() {
+    let scenics = sheets.scenicspots;
+    scenics.forEach(s => {
+        let pic = s.picture;
+        if (files[pic]) {
+            files[pic]++;
+        }
+        else {
+            //rec lack of picture
+            result[pic] = true;
+        }
+    })
+
+    let lacks = Object.keys(result);
+    let lackLen = lacks.length;
+
+    let exts = [];
+    let exLen = 0;
+    for (let k in files) {
+        if (files[k] == 1) {
+            exLen++;
+            exts.push(k);
+        }
+    }
+
+    let str = `总览：缺少资源数->${lackLen}; 没用上的资源数->${exLen};\n缺少资源:\n`;
+    str += lacks.join('\n');
+    str += '\n\n\n没用上的资源:\n'
+    str += exts.join('\n');
+
+    fs.appendFileSync(config.outFile, str, {flag:'w'})
+
+    console.log(`统计结果已经导出到${config.outFile}`)
+}
