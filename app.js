534ebd897a38ab74a87740b306c2e0cece80c856
贺贤明 <82506111@qq.com>
2018-04-20 16:54:41 +0800

    增加mongodb log query mode

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const mongoose = require("mongoose");
 
 module.exports = app => {
     app.beforeStart(async () => {
         // 应用会等待这个函数执行完成才启动
         app.redis.setnx("travel_userid", 1000);
-
+        mongoose.set('debug', true);
     });
 
 };
