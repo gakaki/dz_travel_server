687a1648036d239a43d04b54709fc02db33a1095
贺贤明 <82506111@qq.com>
2018-05-02 14:23:34 +0800

    开放所有生成条件

--- test/app/service/rewardService.test.js
+++ test/app/service/rewardService.test.js
@@ -51,5 +51,25 @@ describe('test reward奖励系统',  () => {
     });
 
 
+    //需要补完
+    it('测试事件任务的配置 测试事件的奖励配置', async () => {
+        // 创建 ctx
+        const ctx       = app.mockContext();
+        const questCfg  = questRepo.find('110006'); //你一路上兴致勃勃地参观，当夕阳西下时，才恋恋不舍地离开。
+        let rewards     = questCfg.getSpotRewardComment();
+        console.log(rewards);
+
+        let cid         = 3;
+        let city        = travelConfig.City.Get(cid);
+        rewards         = questCfg.getSpotRewardComment(city.city);
+        console.log(rewards);
+
+        rewards         = questCfg.getSpotRewardComment(city.city,"这是一个附加配置的讲");
+        console.log(rewards);
+
+
+    });
+
+
 
 });
