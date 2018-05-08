d42a565b40ab5e6617ddced03dfb4d65555b85d3
贺贤明 <82506111@qq.com>
2018-05-08 19:51:17 +0800

    debug合并出错修复

--- debug/debug.js
+++ debug/debug.js
@@ -1,16 +1,8 @@
 module.exports = {
-<<<<<<< HEAD
    EVENTGEN:true,                   //为了调试方便大量生成 1 ，2 ，3 的事件类型 为了方便测试奖励
    EVENTGENINIITAL:true,             //为了调试方便初始化生成10个事件
    SHORTROADMAP:true ,              //短距离路线图 为了测试方便快速走到终点
    QUESTRANDOM:true,                 //quest random for custom 随机事件测试用开关
    QUESTSPECIFIC:false,                 //指定测试事件id
-
-=======
-    EVENTGEN:false,                   //为了调试方便大量生成 1 ，2 ，3 的事件类型 为了方便测试奖励
-    EVENTGENINIITAL:false,             //为了调试方便初始化生成10个事件
-    SHORTROADMAP:false ,              //短距离路线图 为了测试方便快速走到终点
-    QUESTRANDOM:false,                 //quest random for custom 随机事件测试用开关
     WECHATPAY: false,                 //微信支付
->>>>>>> 9dcdcdad20e76483c14cb81bdd15bae44957a716
 }
