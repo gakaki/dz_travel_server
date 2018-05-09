06039960e891d389b2bc5959f7e7b499b7facf53
qianjun <727435849@qq.com>
2018-05-09 17:33:08 +0800

    login

--- app/model/publicModel/userActionRecord.js
+++ app/model/publicModel/userActionRecord.js
@@ -1,11 +1,11 @@
 module.exports = app => {
     const mongoose = app.mongoose;
     const UserActionRecordSchema = new mongoose.Schema({
-        pid: {type: String},
-        appName: {type: String},
-        type: {type: Number, enum: [0, 1, 2, 3, 4, 5]},
-        data: {type: JSON},
-        createDate:{type:Date}
+        pid: { type: String },
+        appName: { type: String },
+        type: { type: Number },
+        data: { type: JSON },
+        createDate: { type: Date },
 
     });
 
