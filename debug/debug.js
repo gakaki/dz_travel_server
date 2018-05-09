4608136e96555a1a7b5eed76c6ca9875d2a1913d
贺贤明 <82506111@qq.com>
2018-05-09 16:41:54 +0800

    初始化增加90个

--- debug/debug.js
+++ debug/debug.js
@@ -1,6 +1,6 @@
 module.exports = {
    EVENTGENTimeShort:false,            //为了调试方便大量生成 1 ，2 ，3 的事件类型 为了方便测试奖励
-   EVENTGENINIITAL:false,             //为了调试方便初始化生成10个事件
+   EVENTGENINIITAL:true,             //为了调试方便初始化生成90个事件
     SHORTROADMAP1:false ,                 //短距离路线图1秒一段路程 为了测试方便快速走到终点
     SHORTROADMAP10:false,               //短距离路线图10秒一段路程 为了测试方便快速走到终点
     SHORTROADMAP30:true,               //短距离路线图30秒一段路程 为了测试方便快速走到终点
