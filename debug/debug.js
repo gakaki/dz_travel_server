22cd4f09010127f593e6865ad6f95c79a664dfee
贺贤明 <82506111@qq.com>
2018-05-10 16:15:43 +0800

    增加02号测试服务器

--- debug/debug.js
+++ debug/debug.js
@@ -7,7 +7,7 @@ module.exports = {
     SHORTROADMAP10:false,               //短距离路线图10秒一段路程 为了测试方便快速走到终点
     SHORTROADMAP30:false,               //短距离路线图30秒一段路程 为了测试方便快速走到终点
     SHORTROADMAP60:false,               //短距离路线图60秒一段路程 为了测试方便快速走到终点
-    
+
     QUESTRANDOM:false,                 //quest random for custom 随机事件测试用开关
     QUESTSPECIFIC:false,                 //指定测试事件id
     WECHATPAY: false,                 //微信支付
