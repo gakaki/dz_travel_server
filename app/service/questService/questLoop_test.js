82b34e6eaf44ef846335bd24e3b92a177300607f
gakaki <gakaki@gmail.lcom>
2018-05-14 07:26:38 +0800

    up

---
+++ app/service/questService/questLoop_test.js
@@ -0,0 +1,26 @@
+const questLoop  = require("./questLoop");
+
+const intervalSecond = 2 * 1000;
+
+const loopFun = () => {
+
+    //初始化变量
+    console.log( intervalSecond );
+
+    //记录playloop上回访问的时间点
+
+    //计算和当前时间具体差了多少来生成事件数量
+
+    //推送到redis中
+
+    ///每次取栈顶的事件
+
+    //同时要注意还有暂停的功能
+
+
+
+
+};
+
+//模拟2秒请求playloop
+setInterval( loopFun ,intervalSecond );
